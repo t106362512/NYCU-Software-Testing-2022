982e8b721b68af85dbf08db42ccf0188209daf41
t106362512 <t106362512@gmail.com>
2022-04-15 16:32:37 +0800

    feat(HW_2): Initialize the project and add documentation
    [skip ci]

---
+++ HW_2/src/test/java/tw/edu/nycu/cs/softwaretesting/spring2022/BoundedQueueTest.java
@@ -0,0 +1,15 @@
+package tw.edu.nycu.cs.softwaretesting.spring2022;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class BoundedQueueTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
