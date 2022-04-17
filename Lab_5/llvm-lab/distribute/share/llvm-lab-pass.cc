74a948cad737aa6b3b41ce45296f8857339f520a
t106362512 <t106362512@gmail.com>
2022-04-17 20:44:54 +0000

    feat(Lab_5): ✨ Add llvm-pass to match lab spec

--- Lab_5/llvm-lab/distribute/share/llvm-lab-pass.cc
+++ Lab_5/llvm-lab/distribute/share/llvm-lab-pass.cc
@@ -43,6 +43,32 @@ bool ExamplePass::runOnModule(Module &M)
   {
 
     /* add you code here */
+
+    // 把要用的 Context 和 Type 搞出乃
+    LLVMContext &C = M.getContext();
+    Type *VoidTy = Type::getVoidTy(C);
+    IntegerType *Int32Ty = IntegerType::getInt32Ty(C);
+
+    if (F.getName() == "main")
+    {
+      // 先挖出 Builder, 以修改行為
+      BasicBlock::iterator IP = F.getEntryBlock().getFirstInsertionPt();
+      IRBuilder<> IRB(&(*IP));
+
+      // 1. 找出沒有回傳值, 且第一個參數是 int 類別的 debug 函式, 並且注入第一個值為 9527 之參數後執行它
+      FunctionType *FnTy = FunctionType::get(VoidTy, {Int32Ty}, false);
+      FunctionCallee Fn = M.getOrInsertFunction("debug", FnTy);
+      IRB.CreateCall(Fn, ConstantInt::get(Int32Ty, 9527));
+
+      // 2. 在確認前, 使 argc 為 9487, 並使輸出符合作業預期結果
+      F.getArg(0)->replaceAllUsesWith(ConstantInt::get(Int32Ty, 9487));
+
+      // 3. 在確認前, 使 argv[1] 為 "aesophor is ghost !!!", 然後存到 Ptr 指定之位址
+      Value *argv1Ptr = IRB.CreateGEP(F.getArg(1), ConstantInt::get(Int32Ty, 1));
+      Value *strPtr = IRB.CreateGlobalStringPtr("aesophor is ghost !!!");
+      IRB.CreateStore(strPtr, argv1Ptr);
+    }
+
     errs() << F.getName() << "\n";
   }
 
